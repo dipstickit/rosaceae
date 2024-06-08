fa1448b24068bdd8455e9a489a1f987d1a6fa8a4
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-06-08 10:25:29 +0700

    fix color logout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../../store/authActions";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Text } from "../../components/Text";
 
 interface Props {
   className?: string;
@@ -140,10 +141,10 @@ export default function Header({ ...props }: Props) {
                 {dropdownOpen && (
                   <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
                     <button
-                      className="block w-full px-4 py-2 text-white bg-gray-100 hover:bg-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                      className="block w-full text-white px-4 py-2 text-white bg-gray-800 hover:bg-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
                       onClick={handleLogout}
                     >
-                      Đăng xuất
+                      <Text className="text-white"> Đăng xuất</Text>
                     </button>
                   </div>
                 )}
