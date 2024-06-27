a9705e0c1467737f47a65ef8291cc5e0417060b3
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-06-27 12:09:59 +0700

    handle UI schedual

--- src/pages/BookingServiceDetail/index.tsx
+++ src/pages/BookingServiceDetail/index.tsx
@@ -1,5 +1,5 @@
 import { Helmet } from "react-helmet";
-import { Button, Heading, Img, SelectBox, Input } from "../../components";
+import { Button, Heading, Img, SelectBox } from "../../components";
 import DateBooking from "../../components/DateBooking";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
@@ -7,16 +7,24 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { SpaLocation } from "../../types/spaLocation.type";
 import BookingSalon from "../BookingSalon";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import TextField from "@mui/material/TextField";
+import dayjs, { Dayjs } from "dayjs";
+
 const dropDownOptions = [
   { label: "Option1", value: "option1" },
   { label: "Option2", value: "option2" },
   { label: "Option3", value: "option3" },
 ];
+
 interface ServiceItem {
   itemId: number;
   itemName: string;
   // Add other properties as needed
 }
+
 const dateBookingData = [
   { time: "13:20" },
   { time: "13:30" },
@@ -27,8 +35,8 @@ export default function BookingServiceDetailPage() {
   const navigate = useNavigate();
   const [receivedData, setReceivedData] = useState<string>("");
   const [searchParams, setSearchParams] = useSearchParams();
-
   const [itemNames, setItemNames] = useState<string[]>([]);
+  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(dayjs());
 
   useEffect(() => {
     const fetchItemTypes = async () => {
@@ -52,12 +60,17 @@ export default function BookingServiceDetailPage() {
 
     fetchItemTypes();
   }, []);
+
   const handleButtonClick = () => {
     navigate("/bookingsalon");
   };
 
   const handleReceiveData = (spa: SpaLocation) => {
     setReceivedData(spa.accountName);
+  };
+
+  const handleDateChange = (newValue: Dayjs | null) => {
+    setSelectedDate(newValue);
   };
 
   return (
@@ -103,41 +116,6 @@ export default function BookingServiceDetailPage() {
               )}
             </Heading>
           </button>
-
-          <div className="container-xs mt-[27px] md:p-5">
-            {/* <div className="gap-[15px] flex sm:flex-col">
-              <Input
-                color="gray_500_01"
-                size="lg"
-                variant="outline"
-                name="iconpinmap"
-                placeholder={`Tim spa gần bạn `}
-                prefix={
-                  <Img
-                    src="images/img_iconpinmap.svg"
-                    alt="icon_pin_map"
-                    className="h-[18px] w-[18px]"
-                  />
-                }
-                className="w-full"
-              />
-              <Button
-                color="gray_500_01"
-                size="7xl"
-                variant="outline"
-                leftIcon={
-                  <Img
-                    src="images/img_iconpinmap.svg"
-                    alt="icon_pin_map"
-                    className="h-[18px] w-[18px]"
-                  />
-                }
-                className="rounded-[10px] gap-[5px] w-full font-bevietnam11 sm:pr-5"
-              >
-                1120, Phạm Văn Đồng, TP Hồ Chí Minh
-              </Button>
-            </div> */}
-          </div>
           <SelectBox
             className="container-xs mt-[27px] border border-solid border-gray-500 font-bevietnam11 font-bold md:p-5 sm:px-5 sm:pt-5"
             shape="round"
@@ -151,59 +129,25 @@ export default function BookingServiceDetailPage() {
             name="chndchv"
             placeholder={`Chọn Dịch Vụ`}
             options={itemNames.map((name) => ({ label: name, value: name }))}
-            // onChange={handleSelectChange}
           />
-          <div className="container-xs mt-[27px] md:p-5">
-            <div className="gap-[15px] flex md:flex-col">
-              <Button
-                color="gray_500_01"
-                size="7xl"
-                variant="outline"
-                className="rounded-[10px] w-full font-bevietnam11 sm:px-5"
-              >
-                Combo làm móng
-              </Button>
-              <Button
-                color="gray_500_01"
-                size="7xl"
-                variant="outline"
-                className="rounded-[10px] w-full font-bevietnam11 sm:px-5"
-              >
-                Làm mụn
-              </Button>
-              <Button
-                color="gray_500_01"
-                size="7xl"
-                variant="outline"
-                className="rounded-[10px] w-full font-bevietnam11 sm:px-5"
-              >
-                Mát xa toàn thân
-              </Button>
-              <Button
-                color="gray_500_01"
-                size="7xl"
-                variant="outline"
-                className="rounded-[10px] w-full font-bevietnam11 sm:px-5"
-              >
-                Nối Mi
-              </Button>
-            </div>
-          </div>
           <div className="container-xs rounded-[10px] pb-[15px] mt-[27px] flex justify-center border border-solid border-gray-500 bg-gray-100_04 px-3.5 pt-3.5 md:p-5">
             <div className="flex w-full items-center justify-between gap-5 sm:flex-col">
               <div className="gap-[15px] flex items-center self-end">
-                <Img
-                  src="images/img_calendar.svg"
-                  alt="calendar"
-                  className="h-[23px] self-start"
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DesktopDatePicker
+                    label="Ngày"
+                    format="MM/DD/YYYY"
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                  />
+                </LocalizationProvider>
+                <TextField
+                  value={selectedDate ? selectedDate.format("MM/DD/YYYY") : ""}
+                  onChange={(e) => {
+                    const newDate = dayjs(e.target.value, "MM/DD/YYYY");
+                    setSelectedDate(newDate.isValid() ? newDate : null);
+                  }}
                 />
-                <Heading
-                  size="lg"
-                  as="h2"
-                  className="self-end !font-bevietnam11"
-                >
-                  Hôm Nay, T3 ( 24/10)
-                </Heading>
               </div>
               <div className="flex items-center gap-5">
                 <Button
