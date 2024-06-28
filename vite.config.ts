0667c393d5de249cddcf303051dcb8419b629b4d
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-06-29 01:08:06 +0700

    handle vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  build: { outDir: "build", }, 
+  build: {
+    outDir: "build",
+  },
   plugins: [tsconfigPaths(), react()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
+  esbuild: {
+    target: "es2021", // hoặc 'esnext' tùy vào nhu cầu của bạn
+  },
 });
-
