0394fe6946210af3b5d3e15a95b1868e4091b776
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-07-12 21:39:54 +0700

    handle Router

--- src/pages/Cart/checkoutItem.tsx
+++ src/pages/Cart/checkoutItem.tsx
@@ -1,6 +1,6 @@
-import { useDispatch } from 'react-redux';
-import { increase, decrease, remove } from './../../store/cartSlice';
-import { Items } from '../../types/Item.model';
+import { useDispatch } from "react-redux";
+import { increase, decrease, remove } from "./../../store/cartSlice";
+import { Items } from "../../types/Item.model";
 import { AiOutlineDelete } from "react-icons/ai";
 
 interface IProductProps {
@@ -8,28 +8,49 @@ interface IProductProps {
 }
 
 const formatPrice = (price: number): string => {
-  return price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+  return price.toLocaleString("vi-VN", { style: "currency", currency: "VND" });
 };
 
 const CheckOutItem = ({ cartItem }: IProductProps) => {
   const dispatch = useDispatch();
 
   return (
-    <div className='flex justify-between items-center border border-solid border-white p-4 mb-10 text-white'>
+    <div className="flex justify-between items-center border border-solid border-white p-4 mb-10 text-white">
       <div className="flex items-center gap-4">
-        <img src={cartItem.itemImages[0]?.imageUrl} alt={cartItem.itemName} className='w-20 h-20 object-cover' />
+        <img
+          src={cartItem.itemImages[0]?.imageUrl}
+          alt={cartItem.itemName}
+          className="w-20 h-20 object-cover"
+        />
       </div>
-      <div className='flex flex-col items-start max-w-[6.8rem]'>
+      <div className="flex flex-col items-start max-w-[6.8rem]">
         <div>{cartItem.itemName}</div>
         <div className="flex items-center gap-4 mt-2">
-          <button className='w-8 h-8 text-primary bg-white rounded-full flex items-center justify-center' onClick={() => dispatch(decrease(cartItem))}>-</button>
-          <div className='text-lg'>{cartItem.quantity}</div>
-          <button className='w-8 h-8 text-primary bg-white rounded-full flex items-center justify-center' onClick={() => dispatch(increase(cartItem))}>+</button>
+          <button
+            className="w-8 h-8 text-primary bg-white rounded-full flex items-center justify-center"
+            onClick={() => dispatch(decrease(cartItem))}
+          >
+            -
+          </button>
+          <div className="text-lg">{cartItem.quantity}</div>
+          <button
+            className="w-8 h-8 text-primary bg-white rounded-full flex items-center justify-center"
+            onClick={() => dispatch(increase(cartItem))}
+          >
+            +
+          </button>
         </div>
       </div>
-      <div className='flex flex-col items-center gap-3'>
-        <AiOutlineDelete className='cursor-pointer text-xl' onClick={() => dispatch(remove(cartItem))} />
-        <div className='text-lg'>{(cartItem.itemPrice * cartItem.quantity) ? formatPrice(cartItem.itemPrice * cartItem.quantity) : '0.0 VND'}</div>
+      <div className="flex flex-col items-center gap-3">
+        <AiOutlineDelete
+          className="cursor-pointer text-xl"
+          onClick={() => dispatch(remove(cartItem))}
+        />
+        <div className="text-lg">
+          {cartItem.itemPrice * cartItem.quantity
+            ? formatPrice(cartItem.itemPrice * cartItem.quantity)
+            : "0.0 VND"}
+        </div>
       </div>
     </div>
   );
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,29 +1,28 @@
-// src/pages/Cart.js
-import { AiOutlineArrowLeft, AiOutlineDelete } from 'react-icons/ai';
-import { useDispatch, useSelector } from 'react-redux';
-import CheckOutItem from './checkoutItem';
-import { RootState } from '../../store/store';
-import { close } from '../../store/CheckOutSlice';
-import { clear } from './../../store/cartSlice';
-import { Link } from 'react-router-dom';
+import { AiOutlineArrowLeft, AiOutlineDelete } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import CheckOutItem from "./checkoutItem";
+import { RootState } from "../../store/store";
+import { close } from "../../store/CheckOutSlice";
+import { clear } from "./../../store/cartSlice";
+import { Link } from "react-router-dom";
 
 const formatPrice = (price: number): string => {
-  return price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+  return price.toLocaleString("vi-VN", { style: "currency", currency: "VND" });
 };
 
 export default function Cart() {
   const dispatch = useDispatch();
-  const { quantity, cartItems, total } = useSelector((state: RootState) => state.cart);
+  const { quantity, cartItems, total } = useSelector(
+    (state: RootState) => state.cart
+  );
 
   return (
     <>
-      {/* Background làm mờ */}
       <div className="bg-black bg-opacity-70 fixed inset-0 z-40"></div>
 
-      {/* Nội dung giỏ hàng */}
       <div className="fixed z-50 top-0 left-0 w-1/3 h-full bg-pastel-200 p-6 rounded-r-lg shadow-lg overflow-y-auto">
         <div className="flex items-center justify-between text-black mb-6">
-          <div 
+          <div
             onClick={() => dispatch(close())}
             className="flex items-center cursor-pointer text-sm text-black"
           >
@@ -34,22 +33,27 @@ export default function Cart() {
         </div>
         <div>
           {cartItems.length === 0 ? (
-            <div className="text-center text-2xl uppercase text-black">Chưa có sản phẩm nào trong giỏ hàng!</div>
+            <div className="text-center text-2xl uppercase text-black">
+              Chưa có sản phẩm nào trong giỏ hàng!
+            </div>
           ) : (
             <>
-              {cartItems.map(cartItem => (
+              {cartItems.map((cartItem) => (
                 <CheckOutItem key={cartItem.itemId} cartItem={cartItem} />
               ))}
               <div className="flex justify-between items-center mt-8 text-black">
-                <div className="text-lg font-bold">Tổng tiền: {formatPrice(total)}</div>
-                <div className="cursor-pointer" onClick={() => dispatch(clear())}>
+                <div className="text-lg font-bold">
+                  Tổng tiền: {formatPrice(total)}
+                </div>
+                <div
+                  className="cursor-pointer"
+                  onClick={() => dispatch(clear())}
+                >
                   <AiOutlineDelete className="text-red-500 text-2xl" />
                 </div>
               </div>
               <Link to="/checkout">
-                <div 
-                  className="text-center bg-white text-primary p-3 mt-8 rounded-xl hover:bg-gray-100 transition duration-300"
-                >
+                <div className="text-center bg-white text-primary p-3 mt-8 rounded-xl hover:bg-gray-100 transition duration-300">
                   Đến trang Thanh Toán
                 </div>
               </Link>
